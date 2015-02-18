7c79b664189bebb35224d784e9b5bc08416422e6
Paróczi Gergő <paroczi@gmail.com>
2015-02-18 23:10:46 +0100

    a

--- Phoebe/src/phoebe/Phoebe.java
+++ Phoebe/src/phoebe/Phoebe.java
@@ -15,7 +15,7 @@ public class Phoebe {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        // proba valtoztata
     }
     
 }
