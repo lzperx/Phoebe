a2483673ddd34117f49cc6e7daf41adbb8193f9b
Muresan73 <modi.g@windowsilve.com>
2015-02-20 11:28:02 +0100

    kifejezőbb név // by Brumi :D

--- Phoebe/src/phoebe/Model/GameObjects.java
+++ Phoebe/src/phoebe/Model/GameObjects.java
@@ -6,14 +6,14 @@ import java.util.List;
 /**
  * Created by Geri on 2015.02.19..
  */
-public class GameElements {
+public class GameObjects {
     private List<Robot> robots;
     private List<Trap> traps;
     private Map map;
 
 
     //Létrehozzuk a megfelelő tárolókat és játék elemeket
-    public GameElements(int width,int high) {
+    public GameObjects(int width, int high) {
         robots = new ArrayList<Robot>();
         traps = new ArrayList<Trap>();
         map = new Map(width, high);
