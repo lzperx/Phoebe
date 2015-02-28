8937c9550b698aa0cbe017fa69e92cf853d37acd
DarkSetis <andris@innosys.hu>
2015-02-28 15:31:11 +0100

    Robotrajzolás
    Cuccok a robotrajzoláshoz

--- Phoebe/src/phoebe/Game.java
+++ Phoebe/src/phoebe/Game.java
@@ -2,6 +2,11 @@ package phoebe;
 
 import phoebe.Control.GameControl;
 import phoebe.Model.GameMapContainer;
+import phoebe.View.GameFrame;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.KeyListener;
 
 public class Game {
         private GameControl gameControl;
@@ -21,6 +26,19 @@ public class Game {
             // Példányosítás
             Game game = new Game();
 
+
+
+            //Robot kirajzolás tesztelése -- András
+            Point test = new Point(300,200);
+            JFrame gameFrame = new JFrame("Phoebe");
+
+            GameFrame frame = new GameFrame(test, "testrobot");
+            gameFrame.add(frame);
+            frame.repaint();
+            gameFrame.setSize(800,600);
+            gameFrame.setVisible(true);
+            gameFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+            //Idáig
 
             // Új játék indítása
             //game.gameControl.startNewGame();
--- Phoebe/src/phoebe/View/GameFrame.java
+++ Phoebe/src/phoebe/View/GameFrame.java
@@ -1,7 +1,49 @@
 package phoebe.View;
 
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+
 /**
  * Created by Geri on 2015.02.24..
  */
-public class GameFrame {
+public class GameFrame extends JComponent {
+
+
+    Dimension robotSize = null;
+    Point robot1 = null;
+    Point robot2 = null;
+
+    BufferedImage testRobot = null;
+
+    public GameFrame(Point robot1Location, String name){
+        robot1 = robot1Location;
+        testRobot = loadRobot(name);
+        robotSize = new Dimension(testRobot.getWidth(),testRobot.getHeight());
+    }
+
+    private BufferedImage loadRobot(String name){
+
+        try{
+            testRobot = ImageIO.read(new File(System.getProperty("user.dir") + File.separator + "robot" + File.separator + name + ".png"));
+        }catch (Exception e){
+            System.out.println("File not found");
+            System.out.println(System.getProperty("user.dir") + File.separator + "robot" + File.separator);
+        }
+        return testRobot;
+    }
+
+    public Dimension getRobotSize(){
+        return robotSize;
+    }
+
+    public void paintComponent(Graphics g){
+
+        g.drawImage(testRobot,100,300,null);
+
+    }
+
 }
+
