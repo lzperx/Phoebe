1b6d37f1ff7c141ba1bf9b827fe49d7e4a580433
Dániel Deák <danid2@freemail.hu>
2015-03-01 14:03:16 +0100

    abstract trap osztály

--- Phoebe/src/phoebe/Model/Trap.java
+++ Phoebe/src/phoebe/Model/Trap.java
@@ -3,7 +3,7 @@ package phoebe.Model;
 /**
  * Created by Muresan73 on 15. 02. 19..
  */
-public class Trap extends GameElements {
+public abstract class Trap extends GameElements {
 
     public Trap(int x, int y, int hitbox) {
         super(x,y,hitbox);
