5d486c428043152be1c184e0795f836675d1fe64
DarkSetis <andris@innosys.hu>
2015-03-07 14:36:27 +0100

    NewGame egyelőre main nélkül

--- Phoebe/src/phoebe/Control/NewGame.java
+++ Phoebe/src/phoebe/Control/NewGame.java
@@ -1,12 +1,19 @@
 package phoebe.Control;
 
+import phoebe.Model.GameMapContainer;
+
 /**
  * Created by andri_000 on 2015.03.07..
  */
 public class NewGame {
+    private int width = 800;
+    private int height = 600;
+
 
    public void initialize(){
 
+       GameMapContainer gameMap = new GameMapContainer(width,height);
+       GameControl controller = new GameControl(gameMap);
    }
 
     public void startGame(){
