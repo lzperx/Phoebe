d14a86e14e43b1f68dfd74abb364aa6b218e21e4
DarkSetis <andris@innosys.hu>
2015-03-07 13:32:29 +0100

    ez
    ez V

--- Phoebe/src/phoebe/Model/vehicleProp.java
+++ Phoebe/src/phoebe/Model/vehicleProp.java
@@ -3,7 +3,7 @@ package phoebe.Model;
 /**
  * Created by andri_000 on 2015.03.07..
  */
-public interface vehicleProp {
+public interface VehicleProp {
 
     public void setSpeed(int speed);
     public void setState(Robot.robotState newState);
